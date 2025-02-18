9a8bf4331134bb7e3f872c94da17ec8b5d21f31f
asd3638 <asd36384@gmail.com>
2025-02-18 14:02:36 +0900

    250218

--- 2352.equal-row-and-column-pairs.py
+++ 2352.equal-row-and-column-pairs.py
@@ -52,16 +52,16 @@
 #
 
 # @lc code=start
+from collections import Counter
 from typing import List
 
 
 class Solution:
-
     def equalPairs(self, grid: List[List[int]]) -> int:
-        row_sum = []
-        for row in grid:
-            row_sum.append(sum(row))  # 각 행의 합을 계산하여 리스트에 추가
-        return row_sum
+        row_count = Counter(tuple(row) for row in grid)
+        col_count = Counter(tuple(col) for col in zip(*grid))
+
+        return sum(row_count[key] * col_count[key] for key in row_count)
 
 
 # @lc code=end
